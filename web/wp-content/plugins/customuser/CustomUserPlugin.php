67020277c9e17284606ffd675e90a1e76bcc73d3
Maria Antonietta Bruno <mariaantonietta.bruno24@gmail.com>
2021-09-07 16:57:23 +0200

    - Project, Task and Subtask Class - "Ente" Form

--- web/wp-content/plugins/customuser/CustomUserPlugin.php
+++ web/wp-content/plugins/customuser/CustomUserPlugin.php
@@ -39,6 +39,11 @@ function extra_user_fields($user)
     <h3>Job information</h3>
     <table class="form-table">
         <tr>
+            <td>Ente</td>
+            <td><input type="text" name="Ente">
+            </td>
+        </tr>
+        <tr>
             <td>Settore</td>
             <td><input type="text" name="Settore">
             </td>
@@ -57,9 +62,11 @@ function extra_user_fields($user)
     <script type="text/javascript">
 
         $('input').addClass('regular-text');
+        $('input[name=Ente]').val('<?php echo get_the_author_meta('Ente', $user->ID); ?>');
         $('input[name=Settore]').val('<?php echo get_the_author_meta('Settore', $user->ID); ?>');
         $('input[name=Area]').val('<?php echo get_the_author_meta('Area', $user->ID); ?>');
         $('input[name=Ufficio]').val('<?php echo get_the_author_meta('Ufficio', $user->ID); ?>');
+
 
     </script>
     <pre>
@@ -87,6 +94,7 @@ function save_extra_user_field($user_id)
     update_user_meta($user_id, 'Settore', $_POST["Settore"]);
     update_user_meta($user_id, 'Area', $_POST["Area"]);
     update_user_meta($user_id, 'Ufficio', $_POST["Ufficio"]);
+    update_user_meta($user_id, 'Ente', $_POST["Ente"]);
 
 
 }
@@ -188,7 +196,7 @@ class User
     {
         $conn = new Connection();
         $mysqli = $conn->connect();
-        print_r($this->idKanboard);
+
 
         if ($this->idKanboard != NULL) {
             $sql = "UPDATE users SET name=?, email=? WHERE id=?";
@@ -226,7 +234,281 @@ class User
     }
 }
 
+class Project{
+    private $project_name;
+    private $id_project;
+    public function __construct()
+    {
 
+    }
+
+
+    public function getProjectName()
+    {
+        return $this->project_name;
+    }
+
+
+    public function setProjectName($project_name)
+    {
+        $this->project_name = $project_name;
+    }
+
+
+    public function getIdProject()
+    {
+        return $this->id_project;
+    }
+
+
+    public function setIdProject($id_project)
+    {
+        $this->id_project = $id_project;
+    }
+public function updateProject(){
+    $conn = new Connection();
+    $mysqli = $conn->connect();
+    if($this->id_project!=NULL){
+        $sql = "UPDATE projects SET name=? WHERE id=?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("si", $this->project_name, $this->id_project);
+        $res = $stmt->execute();
+    }
+    else{
+        $sql = "INSERT INTO projects (name) VALUES(?)";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("s", $this->project_name);
+        $res = $stmt->execute();
+    }
+    $mysqli->close();
+}
+    public function deleteProject($idProject)
+    {
+        $conn = new Connection();
+        $mysqli = $conn->connect();
+        $this->setIdProject($idProject);
+        $sql = "DELETE  FROM projects WHERE id=?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("i", $this->id_project);
+        $res = $stmt->execute();
+        $mysqli->close();
+    }
+}
+class Task{
+    private $id_task;
+    private $title;
+    private $description;
+    private $date_creation;
+    private $date_completed;
+    public function __construct()
+    {
+
+    }
+
+
+    public function getIdTask()
+    {
+        return $this->id_task;
+    }
+
+
+    public function setIdTask($id_task)
+    {
+        $this->id_task = $id_task;
+    }
+
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+
+    public function getDateCreation()
+    {
+        return $this->date_creation;
+    }
+
+
+    public function setDateCreation($date_creation)
+    {
+        $this->date_creation = $date_creation;
+    }
+
+
+    public function getDateCompleted()
+    {
+        return $this->date_completed;
+    }
+
+
+    public function setDateCompleted($date_completed)
+    {
+        $this->date_completed = $date_completed;
+    }
+    public function updateTask(){
+        $conn = new Connection();
+        $mysqli = $conn->connect();
+        if($this->id_task!=NULL){
+            $sql = "UPDATE tasks SET title=?,description=?,date_creation=?,date_completed=? WHERE id=?";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("ssiii", $this->title,$this->description,$this->date_creation,$this->date_completed, $this->id_task);
+            $res = $stmt->execute();
+        }
+        else{
+            $sql = "INSERT INTO tasks (title,description,date_creation,date_completed) VALUES(?,?,?,?)";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("ssii", $this->title,$this->description,$this->date_creation,$this->date_completed);
+            $res = $stmt->execute();
+        }
+        $mysqli->close();
+    }
+    public function deleteTask($idTask)
+    {
+        $conn = new Connection();
+        $mysqli = $conn->connect();
+        $this->setIdTask($idTask);
+        $sql = "DELETE  FROM tasks WHERE id=?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("i", $this->id_task);
+        $res = $stmt->execute();
+        $mysqli->close();
+    }
+
+
+}
+
+class SubTask{
+
+    private $id_subtask;
+    private $title;
+    private $status;
+    private $time_estimated;
+    private $time_spent;
+    private $position;
+    public function __construct()
+    {
+
+    }
+
+    public function getIdSubtask()
+    {
+        return $this->id_subtask;
+    }
+
+
+    public function setIdSubtask($id_subtask)
+    {
+        $this->id_subtask = $id_subtask;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    public function getTimeEstimated()
+    {
+        return $this->time_estimated;
+    }
+
+
+    public function setTimeEstimated($time_estimated)
+    {
+        $this->time_estimated = $time_estimated;
+    }
+
+
+    public function getTimeSpent()
+    {
+        return $this->time_spent;
+    }
+
+
+    public function setTimeSpent($time_spent)
+    {
+        $this->time_spent = $time_spent;
+    }
+
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+    public function setDateCompleted($date_completed)
+    {
+        $this->date_completed = $date_completed;
+    }
+    public function updateSubTask(){
+        $conn = new Connection();
+        $mysqli = $conn->connect();
+        if($this->id_subtask!=NULL){
+            $sql = "UPDATE subtasks SET title=?,status=?,time_estimated=?,time_spent=?, position=? WHERE id=?";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("siiiii", $this->title,$this->status,$this->time_estimated,$this->time_spent, $this->position, $this->id_subtask);
+            $res = $stmt->execute();
+        }
+        else{
+            $sql = "INSERT INTO subtasks (title,status,time_estimated,time_spent,position ) VALUES(?,?,?,?,?)";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("siiii", $this->title,$this->status,$this->time_estimated,$this->time_spent,$this->position);
+            $res = $stmt->execute();
+        }
+        $mysqli->close();
+    }
+    public function deleteSubTask($idSubtask)
+    {
+        $conn = new Connection();
+        $mysqli = $conn->connect();
+        $this->setIdSubtask($idSubtask);
+        $sql = "DELETE  FROM subtasks WHERE id=?";
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("i", $this->id_subtask);
+        $res = $stmt->execute();
+        $mysqli->close();
+    }
+
+}
 ?>
 
 
